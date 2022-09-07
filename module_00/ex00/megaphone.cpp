4119e49882db3833a91b71abff72b4cfb298964c
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-09-07 11:12:40 +0100

    added ex00 megaphone.cpp

---
+++ module_00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/06 14:56:21 by dsilveri          #+#    #+#             */
+/*   Updated: 2022/09/07 10:40:20 by dsilveri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+    char teste;
+    int i;
+    int j;
+
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else if (argc > 1)
+    {
+        i = 1;
+        while (i < argc)
+        {
+            j = 0;
+            while(argv[i][j])
+            {
+                std::cout << (char)toupper(argv[i][j]);
+                j++;
+            }
+            i++;
+        }
+    }
+    std::cout << std::endl;
+    return (0);
+}
