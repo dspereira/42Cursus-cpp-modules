3fe1bfc099f28274ce36973bc1b73ba29df8a6cb
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-09-23 14:37:05 +0100

    update m01 ex05

--- module_01/ex05/src/Harl.cpp
+++ module_01/ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 15:29:18 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/09/23 10:46:52 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/09/23 14:32:03 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,43 @@ Harl::Harl(void)
 
 void Harl::debug(void)
 {
-	std::cout 
-		<< "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+	std::cout
+		<< "[ DEBUG ]"
+		<< std::endl
+		<< "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
+		<< std::endl
 		<< std::endl;
 }
 
 void Harl::info(void)
 {
-	std::cout 
-		<< "INFO: I cannot believe adding extra bacon costs more money."
+	std::cout
+		<< "[ INFO ]"
+		<< std::endl 
+		<< "I cannot believe adding extra bacon costs more money."
 		<< "You didn’t put enough bacon in my burger!If you did, I wouldn’t be asking for more!"
+		<< std::endl
 		<< std::endl;
 }
 
 void Harl::warning(void)
 {
-	std::cout 
-		<< "WARNING: I think I deserve to have some extra bacon for free."
+	std::cout
+		<< "[ WARNING ]"
+		<< std::endl
+		<< "I think I deserve to have some extra bacon for free."
 		<< "I’ve been coming for years whereas you started working here since last month."
+		<< std::endl
 		<< std::endl;
 }
 
 void Harl::error(void)
 {
-	std::cout 
-		<< "ERROR: This is unacceptable! I want to speak to the manager now."
+	std::cout
+		<< "[ ERROR ]"
+		<< std::endl
+		<< "This is unacceptable! I want to speak to the manager now."
+		<< std::endl
 		<< std::endl;
 }
 
--- module_01/ex05/src/main.cpp
+++ module_01/ex05/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 15:32:32 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/09/23 10:49:33 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/09/23 14:33:08 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,8 @@ int main(void)
     Harl harl;
 
     harl.complain("DEBUG");
-    std::cout << std::endl;
     harl.complain("INFO");
-    std::cout << std::endl;
     harl.complain("WARNING");
-    std::cout << std::endl;
     harl.complain("ERROR");
     return (0);
 }
