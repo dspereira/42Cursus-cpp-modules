d4a2f81ea41c9513b24ae082b75f05fb9e962714
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-10-03 14:10:17 +0100

    Update m03 ex01

--- module_03/ex01/src/ScavTrap.hpp
+++ module_03/ex01/src/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 09:42:59 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/10/03 12:34:56 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/10/03 14:08:59 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,10 @@
 
 class ScavTrap: public ClapTrap
 {
-	private:
-	
 	public:
 		ScavTrap();
 		ScavTrap(std::string name);
 		~ScavTrap();
-
 		void attack(const std::string& target);
 		void guardGate();
 };
