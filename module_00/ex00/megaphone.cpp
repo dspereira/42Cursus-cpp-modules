a8e08bcfea5c8b0f50dbafc6fff11714d3cea7d0
Diogo Silverio pereira <dsilveri@c2r9s8.42lisboa.com>
2022-10-18 16:15:03 +0100

    Updated m00 ex00

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:56:21 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/09/07 11:24:51 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/10/18 16:13:53 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 
 int main(int argc, char **argv)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else if (argc > 1)
-    {
-        i = 1;
-        while (i < argc)
-        {
-            j = 0;
-            while(argv[i][j])
-            {
-                std::cout << (char)toupper(argv[i][j]);
-                j++;
-            }
-            i++;
-        }
-    }
-    std::cout << std::endl;
-    return (0);
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			j = 0;
+			while(argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl; 
+	}
+	return (0);
 }
