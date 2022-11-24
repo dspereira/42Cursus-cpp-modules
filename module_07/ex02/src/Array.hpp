a550525290ba0186ab4518102eb61203a5069e8e
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-11-24 16:05:23 +0000

    update m07 ex02

--- module_07/ex02/src/Array.hpp
+++ module_07/ex02/src/Array.hpp
@@ -3,65 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 11:12:06 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/11/17 13:00:18 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/11/24 15:48:31 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+
 template<typename T>
 class Array
 {
 	private:
-	T	*arr;
-	int	size;
+	unsigned int	_size;
+	T				*arr;
 
 	public:
-	Array(): arr(new T()), size(0)
-	{
-		arr[0] = 0;
-	}
+	Array(): _size(0), arr(new T[0]) {}
 
-	Array(int n): arr(new T[n]), size(n)
-	{	
-		//arr = new T[n];
-		for(int i = 0; i < n; i++)
-			arr[i] = 0;
-	}
+	Array(unsigned int size):_size(size), arr(new T[size]) {}
 
-	Array(const Array& other)
+	Array(const Array& other): _size(other._size), arr(new T[_size])
 	{
-		size = other.size;
-		arr = new T[size];
-		for(int i = 0; i < size; i++)
+		for(unsigned int i = 0; i < _size; i++)
 			arr[i] = other.arr[i];
 	}
-	
+
 	~Array()
 	{
 		delete[] arr;
 	}
-	
+
 	Array& operator=(const Array& other)
 	{
 		if (this != &other)
 		{
 			delete[] arr;
-			size = other.size;
-			arr = new T[size];
-			for(int i = 0; i < size; i++)
+			_size = other._size;
+			arr = new T[_size];
+			for(unsigned int i = 0; i < _size; i++)
 				arr[i] = other.arr[i];
 		}
 		return (*this);
 	}
-	T& operator[](int n)
+
+	unsigned int size(void) const
 	{
-		if (n >= 0 && n < size)
-			return (arr[n]);
+		return(_size);
+	} 
+
+	// write
+	T& operator[](unsigned int idx)
+	{
+		if (idx < _size)
+			return (arr[idx]);
+		else 
+			throw std::exception();
+	}
+
+	// read
+    const T& operator[](unsigned int idx) const 
+	{ 
+		if (idx < _size)
+			return (arr[idx]);
 		else 
 			throw std::exception();
 	}
--- module_07/ex02/src/main.cpp
+++ module_07/ex02/src/main.cpp
@@ -3,31 +3,99 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 11:30:54 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/11/17 13:01:33 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/11/24 16:04:07 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Array.hpp"
 
+void test_wrong_index(void)
+{
+	Array<int> arr(3);
+
+	try	{
+		arr[0] = 10;
+		arr[1] = 20;
+		std::cout << arr.size() << std::endl;
+		std::cout << arr[0] << std::endl;
+		std::cout << arr[1] << std::endl;
+		std::cout << arr[5] << std::endl;
+		
+	}
+	catch (std::exception &e){
+		std::cout << "Index errado" << std::endl;
+	}
+}
+
+template<typename T>
+void printArr(Array<T> arr)
+{
+	for (unsigned int i = 0; i < arr.size(); i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+}
+
+
+void test_copy(void)
+{
+	Array<int> arr1(2);
+	arr1[0] = 10;
+	arr1[1] = 20;
+	Array<int> arr2(arr1);
+	Array<int> arr3;
+	
+	arr3 = arr1;
+
+	std::cout << "Array 1" << std::endl;
+	printArr(arr1);
+	std::cout << "Array 2" << std::endl;
+	printArr(arr2);
+	std::cout << "Array 3" << std::endl;
+	printArr(arr3);
+
+	arr1[0] = 300;
+	arr1[1] = 400;
+	std::cout << std::endl;
+	std::cout << "Array 1 após alteração" << std::endl;
+	std::cout << "Array 1" << std::endl;
+	printArr(arr1);
+	std::cout << "Array 2" << std::endl;
+	printArr(arr2);
+	std::cout << "Array 3" << std::endl;
+	printArr(arr3);
+	
+}
+
+class A{
+	public:
+	A(){
+		std::cout << "Default constructor" << std::endl;
+	}
+	~A(){
+		std::cout << "Destructor" << std::endl;
+	}
+};
+
+void testClass(void)
+{
+	Array<A> arr(3);
+}
+
+
 int main(void)
 {
-	Array<int> a(11);
-
-	try{
-		a[20] = 20;
-	}
-	catch (std::exception &e)
-	{
-		std::cout << "exeption" << std::endl;
-		e.what();
-		//std::cout << a[10] << std::endl;
-		//std::cout << a[10] << std::endl;
-	}
-	//std::cout << a[10] << std::endl;
-
-	return (0);
+	std::cout << "test_wrong_index" << std::endl;
+	test_wrong_index();
+	std::cout << std::endl;
+	std::cout << "test copy" << std::endl;
+	test_copy();
+	std::cout << std::endl;
+	std::cout << "test Class" << std::endl;
+	testClass();
 }
