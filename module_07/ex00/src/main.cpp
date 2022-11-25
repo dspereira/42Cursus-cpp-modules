01fbc5a328e8253beac83f743bc971dfbb49dd08
Diogo Silverio pereira <dsilveri@c1r18s2.42lisboa.com>
2022-11-25 11:36:58 +0000

    m07 update ex00

--- module_07/ex00/src/main.cpp
+++ module_07/ex00/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 17:12:40 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/11/16 17:12:58 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/11/25 11:15:53 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,29 @@ void tests(void)
 	float a1 = 10.5f;
 	float b1 = 5.6f;
 
-	swap<int>(a, b);
+	
 	std::cout << "INT" << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << max<int>(a, b) << std::endl;
-	std::cout << min<int>(a, b) << std::endl;
+	std::cout << "a:" << a << "  b:" << b << std::endl;
+	std::cout << "max:" << max<int>(a, b) << std::endl;
+	std::cout << "min:" << min<int>(a, b) << std::endl;
+	swap<int>(a, b);
+	std::cout << "swap\na" << a << "  b:" << b << std::endl;
 	std::cout << std::endl;
 
-	swap<float>(a1, b1);
 	std::cout << "FLOAT" << std::endl;
-	std::cout << a1 << std::endl;
-	std::cout << b1 << std::endl;
-	std::cout << max<float>(a1, b1) << std::endl;
-	std::cout << min<float>(a1, b1) << std::endl;
+	std::cout << "a:" << a1 << " b:" << b1 << std::endl;
+	std::cout << "max:" << max<int>(a1, b1) << std::endl;
+	std::cout << "min:" << min<int>(a1, b1) << std::endl;
+	swap<float>(a1, b1);
+	std::cout << "swap\na " << a1 << "  b: " << b1 << std::endl;
+	std::cout << std::endl;
 }
 
-int main(void) 
+void test_subject(void)
 {
 	int a = 2;
 	int b = 3;
 
-	//tests();
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
@@ -54,5 +55,11 @@ int main(void)
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+}
+
+int main(void) 
+{
+	//tests();
+	test_subject();
 	return 0;
 }
