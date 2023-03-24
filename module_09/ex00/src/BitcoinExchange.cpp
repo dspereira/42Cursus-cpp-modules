222bc56e1c65689fba5f154817b5d7409e0bf9df
Diogo Pereira <diogo.pereira.ee@gmail.com>
2023-03-24 11:36:29 +0000

    module_09 ex00 added date validation

--- module_09/ex00/src/BitcoinExchange.cpp
+++ module_09/ex00/src/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 10:20:57 by dsilveri          #+#    #+#             */
-/*   Updated: 2023/03/23 17:26:19 by dsilveri         ###   ########.fr       */
+/*   Updated: 2023/03/24 11:34:51 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,20 +62,16 @@ void BitcoinExchange::displayDateValue(std::string line)
 	std::string 							date;
 	float									value;
 	float									total;
-	std::map<std::string, double>::iterator	it;
+	float									btcPrice;
 
+	// getDateValueInput change name
 	if (getDateValue(line, date, &value) == -1)
 		return ;
-	it = bitcoinPrices.lower_bound(date);
+	btcPrice = getBitcoinPrices(date);
+	std::cout << "teste: " << btcPrice << std::endl;
 
-
-
-	std::cout << date << " => ";
-	if (it->first.compare(date) == 0)
-		total = value * it->second;
-	else
-		total = value * (--it)->second;
-	std::cout << value << " = " << total << std::endl;
+	total = btcPrice * value;
+	std::cout << date << " => " << value << " = " << total << std::endl;
 }
 
 int BitcoinExchange::getDateValue(std::string line, std::string &date, float *value)
@@ -100,5 +96,58 @@ int BitcoinExchange::getDateValue(std::string line, std::string &date, float *va
 		return (-1);
 	}
 	date = line.substr(0, pos - 1);
+	if (!isValidDate(date))
+		displayError("not a valid date.", NULL);
 	return (0);
+}
+
+float BitcoinExchange::getBitcoinPrices(std::string date)
+{
+	std::map<std::string, double>::iterator	it, end, begin;
+
+	it = bitcoinPrices.lower_bound(date);
+	begin = bitcoinPrices.begin();
+	end = --bitcoinPrices.end();
+
+	if (date.compare(begin->first) < 0)
+		return (0);
+	if (date.compare(end->first) > 0)
+		return (end->second);
+	if (it->first.compare(date) == 0)
+		return (it->second);
+	return ((--it)->second);
+}
+
+int BitcoinExchange::isValidDate(std::string date)
+{
+	size_t fDash;
+	size_t sDash;
+	int	   y;
+	int	   m;
+	int	   d;
+
+	fDash = date.find('-');
+	sDash = date.rfind('-');
+	if (fDash == std::string::npos || sDash == std::string::npos)
+		return (0);
+	y = std::atoi(date.substr(0, fDash).c_str());
+	m = std::atoi(date.substr(fDash + 1, sDash).c_str());
+	d = std::atoi(date.substr(sDash + 1).c_str());
+	if (m < 0 || m > 12)
+		return (0);
+	if (d < 0 || d > 31)
+		return (0);
+    if ((m == 4 || m == 6 || m == 9 || m == 11) && d > 30)
+        return (0);
+	if (((y % 4 == 0 && y % 100 != 0) || y % 400 == 0))
+	{
+		if (m == 2 && d > 29)
+			return (0);
+	}
+	else
+	{
+		if (m == 2 && d > 28)
+			return (0);
+	}
+	return (1);
 }
--- module_09/ex00/src/BitcoinExchange.hpp
+++ module_09/ex00/src/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 10:22:00 by dsilveri          #+#    #+#             */
-/*   Updated: 2023/03/23 17:30:37 by dsilveri         ###   ########.fr       */
+/*   Updated: 2023/03/24 11:34:49 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ class BitcoinExchange
 		void	displayError(std::string error, std::string *param);
 		void	displayDateValue(std::string line);
 		int		getDateValue(std::string line, std::string &date, float *value);
-		//void	getBitcoinPrices(std::string date);
-
+		float	getBitcoinPrices(std::string date);
+		int		isValidDate(std::string date);
 
 	public:
 				BitcoinExchange();
